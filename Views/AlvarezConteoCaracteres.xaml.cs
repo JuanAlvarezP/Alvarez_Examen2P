c365ca3745364fca7e27cd9ecb0e6715246574b9
Juan Andres Alvarez Pullas <juan.alvarez.pullas@udla.edu.ec>
2023-12-06 08:27:43 -0500

    Cambios en la nueva página para ordenar resultados

--- Views/AlvarezConteoCaracteres.xaml.cs
+++ Views/AlvarezConteoCaracteres.xaml.cs
@@ -11,21 +11,41 @@ public partial class AlvarezConteoCaracteres : ContentPage
     {
         string inputText = txtInput.Text;
 
-        
+        // Realizar cálculos
         int numbersCount = CountNumbers(inputText);
         int lettersCount = CountLetters(inputText);
-       
-        lblNumbers.Text = $"Números: {numbersCount}";
-        lblLetters.Text = $"Letras: {lettersCount}";
+        int vowelsCount = CountVowels(inputText);
+        int upperCaseCount = CountUpperCase(inputText);
+        int lowerCaseCount = CountLowerCase(inputText);
+        int totalCount = inputText.Length;
+
+        // Actualizar las etiquetas con los resultados
+        Alvarez_Numeros.Text = $"Números: {numbersCount}";
+        Alvarez_Letras.Text = $"Letras: {lettersCount}";
+        Alvarez_Vocales.Text = $"Vocales: {vowelsCount}";
+        Alvarez_Mayus.Text = $"Mayúsculas: {upperCaseCount}";
+        Alvarez_Minus.Text = $"Minúsculas: {lowerCaseCount}";
+        Alvarez_Total.Text = $"Total: {totalCount}";
+
+        // Mostrar el layout de resultados
+        Resultados.IsVisible = true;
     }
 
     private void OnClearClicked(object sender, EventArgs e)
     {
+        // Limpiar campos y ocultar el layout de resultados
         txtInput.Text = string.Empty;
-        lblNumbers.Text = string.Empty;
-        lblLetters.Text = string.Empty;
+        Alvarez_Numeros.Text = string.Empty;
+        Alvarez_Letras.Text = string.Empty;
+        Alvarez_Vocales.Text = string.Empty;
+        Alvarez_Mayus.Text = string.Empty;
+        Alvarez_Minus.Text = string.Empty;
+        Alvarez_Total.Text = string.Empty;
+
+        Resultados.IsVisible = false;
     }
 
+    // Métodos para realizar los cálculos
     private int CountNumbers(string text)
     {
         return text.Count(char.IsDigit);
@@ -36,4 +56,18 @@ public partial class AlvarezConteoCaracteres : ContentPage
         return text.Count(char.IsLetter);
     }
 
+    private int CountVowels(string text)
+    {
+        return text.Count(c => "AEIOUaeiou".Contains(c));
+    }
+
+    private int CountUpperCase(string text)
+    {
+        return text.Count(char.IsUpper);
+    }
+
+    private int CountLowerCase(string text)
+    {
+        return text.Count(char.IsLower);
+    }
 }
