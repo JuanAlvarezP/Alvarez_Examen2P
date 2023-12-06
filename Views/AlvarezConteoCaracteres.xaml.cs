32dbac728f2b3d21a26a9913a6df6d38a9ff9888
Juan Andres Alvarez Pullas <juan.alvarez.pullas@udla.edu.ec>
2023-12-06 07:57:43 -0500

    Modificación de la nueva página para incluir todas las funcionalidades y referenciarla en el AppShell

--- Views/AlvarezConteoCaracteres.xaml.cs
+++ Views/AlvarezConteoCaracteres.xaml.cs
@@ -6,4 +6,34 @@ public partial class AlvarezConteoCaracteres : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void OnCalculateClicked(object sender, EventArgs e)
+    {
+        string inputText = txtInput.Text;
+
+        
+        int numbersCount = CountNumbers(inputText);
+        int lettersCount = CountLetters(inputText);
+       
+        lblNumbers.Text = $"Números: {numbersCount}";
+        lblLetters.Text = $"Letras: {lettersCount}";
+    }
+
+    private void OnClearClicked(object sender, EventArgs e)
+    {
+        txtInput.Text = string.Empty;
+        lblNumbers.Text = string.Empty;
+        lblLetters.Text = string.Empty;
+    }
+
+    private int CountNumbers(string text)
+    {
+        return text.Count(char.IsDigit);
+    }
+
+    private int CountLetters(string text)
+    {
+        return text.Count(char.IsLetter);
+    }
+
 }
